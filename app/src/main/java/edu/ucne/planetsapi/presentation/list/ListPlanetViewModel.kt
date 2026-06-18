c3624ab540975ad0d23af5987c4afff66f831b7e
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-17 23:05:27 -0400

    Crea viewmodel de la lista ListPlanetViewModel

---
+++ app/src/main/java/edu/ucne/planetsapi/presentation/list/ListPlanetViewModel.kt
@@ -0,0 +1,81 @@
+package edu.ucne.planetsapi.presentation.list
+
+import androidx.compose.runtime.currentComposer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.planetsapi.data.remote.Resource
+import edu.ucne.planetsapi.domain.usecase.GetPlanetUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ListPlanetViewModel @Inject constructor(
+    private val getPlanetsUseCase: GetPlanetUseCase
+): ViewModel() {
+    private val _state = MutableStateFlow(ListPlanetUiState())
+    val state = _state.asStateFlow()
+
+    init {
+        onLoad()
+    }
+
+    fun onEvent(event: ListPlanetUiEvent) {
+        when (event) {
+            is ListPlanetUiEvent.UpdateFilters -> {
+                _state.update {
+                    it.copy(
+                        nameFilter = event.name,
+                        isDestroyedFilter = event.isDestroyed
+                    )
+                }
+            }
+
+            ListPlanetUiEvent.Search -> onLoad()
+        }
+    }
+
+    fun onLoad() {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(
+                    isLoading = true
+                )
+            }
+
+            val current = _state.value
+            val result = getPlanetsUseCase(
+                name = current.nameFilter.takeIf { it.isNotBlank() },
+                isDestroyed = current.isDestroyedFilter
+            )
+
+            when (result) {
+                is Resource.Success ->
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            planets = result.data ?: emptyList()
+                        )
+                    }
+
+                is Resource.Error ->
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            error = result.message
+                        )
+                    }
+
+                is Resource.Loading ->
+                    _state.update {
+                        it.copy(
+                            isLoading = true
+                        )
+                    }
+            }
+        }
+    }
+}
