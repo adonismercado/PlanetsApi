7763243fc950dd7db8e001d11a1773872651cf5e
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-18 00:04:11 -0400

    Agrega la navegacion en el MainActivity

--- app/src/main/java/edu/ucne/planetsapi/MainActivity.kt
+++ app/src/main/java/edu/ucne/planetsapi/MainActivity.kt
@@ -11,37 +11,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.planetsapi.presentation.navigation.AppNavHost
 import edu.ucne.planetsapi.ui.theme.PlanetsApiTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PlanetsApiTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navHostController = rememberNavController()
+                AppNavHost(navHostController = navHostController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PlanetsApiTheme {
-        Greeting("Android")
     }
 }
