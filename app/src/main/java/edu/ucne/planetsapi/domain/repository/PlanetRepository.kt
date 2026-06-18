aa555150919eec2820d6bcc42bd4531c3c4b4d23
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-17 22:42:49 -0400

    Crea interfaz del repositorio PlanetRepository

---
+++ app/src/main/java/edu/ucne/planetsapi/domain/repository/PlanetRepository.kt
@@ -0,0 +1,15 @@
+package edu.ucne.planetsapi.domain.repository
+
+import edu.ucne.planetsapi.data.remote.Resource
+import edu.ucne.planetsapi.data.remote.dto.PlanetDto
+
+interface PlanetRepository {
+    suspend fun getPlantes(
+        page: Int,
+        limit: Int,
+        name: String?,
+        isDestroyed: Boolean?
+    ): Resource<List<PlanetDto>>
+
+    suspend fun getPlanetDetail(id: Int): Resource<PlanetDto>
+}
