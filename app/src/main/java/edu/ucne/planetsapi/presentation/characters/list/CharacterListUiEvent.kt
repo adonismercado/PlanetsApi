1e70509f47cb5c55999bf49a6ec69121279d505c
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-25 19:06:59 -0400

    Corrige nombre de metodo UpdateFilters en CharacterListUiEvent

--- app/src/main/java/edu/ucne/planetsapi/presentation/characters/list/CharacterListUiEvent.kt
+++ app/src/main/java/edu/ucne/planetsapi/presentation/characters/list/CharacterListUiEvent.kt
@@ -1,7 +1,7 @@
 package edu.ucne.planetsapi.presentation.characters.list
 
 interface CharacterListUiEvent {
-    data class UpdateFIlters(
+    data class UpdateFilters(
         val name: String,
         val gender: String,
         val race: String,
