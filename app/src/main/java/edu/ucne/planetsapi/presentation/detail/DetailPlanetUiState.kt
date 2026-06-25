827bd092978712386c862fbf672faaaba0b342be
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-25 18:07:37 -0400

    cambia tipo de dato del planet de de DTO a modelo en el estadod e detalle

--- app/src/main/java/edu/ucne/planetsapi/presentation/detail/DetailPlanetUiState.kt
+++ app/src/main/java/edu/ucne/planetsapi/presentation/detail/DetailPlanetUiState.kt
@@ -1,9 +1,10 @@
 package edu.ucne.planetsapi.presentation.detail
 
 import edu.ucne.planetsapi.data.remote.dto.PlanetDto
+import edu.ucne.planetsapi.domain.model.Planet
 
 data class DetailPlanetUiState (
     val isLoading: Boolean = false,
-    val planet: PlanetDto? = null,
+    val planet: Planet? = null,
     val error: String? = null
 )
