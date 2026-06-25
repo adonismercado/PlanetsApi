46b3606b0a2f6a91b7b78856af009cdf9c117221
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-25 18:32:34 -0400

    Agrega metodos para obtener lista de Characters y el detalle de un Character en la interfaz de la API DragonBallApi

--- app/src/main/java/edu/ucne/planetsapi/data/remote/DragonBallApi.kt
+++ app/src/main/java/edu/ucne/planetsapi/data/remote/DragonBallApi.kt
@@ -1,5 +1,7 @@
 package edu.ucne.planetsapi.data.remote
 
+import edu.ucne.planetsapi.data.remote.dto.CharacterDto
+import edu.ucne.planetsapi.data.remote.dto.CharacterResponseDto
 import edu.ucne.planetsapi.data.remote.dto.PlanetDto
 import edu.ucne.planetsapi.data.remote.dto.PlanetResponseDto
 import retrofit2.Response
@@ -20,4 +22,18 @@ interface DragonBallApi {
     suspend fun getPlanetDetail(
         @Path("id") id: Int
     ): Response<PlanetDto>
+
+    @GET("characters")
+    suspend fun getCharacters(
+        @Query("page") page: Int,
+        @Query("limit") limit: Int,
+        @Query("name") name: String?,
+        @Query("gender") gender: String?,
+        @Query("race") race: String?
+    ): Response<CharacterResponseDto>
+
+    @GET("characters/{id}")
+    suspend fun getCharacterDetail(
+        @Path("id") id: Int
+    ): Response<CharacterDto>
 }
