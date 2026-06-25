bf5eba4805486c793d15776208344f7ebe475f69
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-25 19:29:10 -0400

    Agrega navegacion entre pantallas de character en el AppNavHost

--- app/src/main/java/edu/ucne/planetsapi/presentation/navigation/AppNavHost.kt
+++ app/src/main/java/edu/ucne/planetsapi/presentation/navigation/AppNavHost.kt
@@ -1,10 +1,17 @@
 package edu.ucne.planetsapi.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import edu.ucne.planetsapi.presentation.characters.detail.CharacterDetailScreen
+import edu.ucne.planetsapi.presentation.characters.detail.CharacterDetailViewModel
+import edu.ucne.planetsapi.presentation.characters.list.CharacterListScreen
 import edu.ucne.planetsapi.presentation.planets.detail.DetailPlanetScreen
+import edu.ucne.planetsapi.presentation.planets.detail.DetailPlanetViewModel
 import edu.ucne.planetsapi.presentation.planets.list.ListPlanetScreen
 
 @Composable
@@ -24,7 +31,31 @@ fun AppNavHost(
         }
 
         composable<Screen.PlanetDetail> {
+            val viewModel: DetailPlanetViewModel = hiltViewModel()
+            val state by viewModel.state.collectAsStateWithLifecycle()
+
             DetailPlanetScreen(
+                state = state,
+                onBack = {
+                    navHostController.navigateUp()
+                }
+            )
+        }
+
+        composable<Screen.PlanetList> {
+            CharacterListScreen(
+                onCharacterClick = { characterId ->
+                    navHostController.navigate(Screen.CharacterDetail(characterId))
+                }
+            )
+        }
+
+        composable<Screen.CharacterDetail> {
+            val viewModel: CharacterDetailViewModel = hiltViewModel()
+            val state by viewModel.state.collectAsStateWithLifecycle()
+
+            CharacterDetailScreen(
+                state = state,
                 onBack = {
                     navHostController.navigateUp()
                 }
