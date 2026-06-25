19b20f44d7e902dab951f162592c1efa7f666a95
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-06-25 18:52:21 -0400

    Crea viewmodel de lista de characters CharacterListViewModel

---
+++ app/src/main/java/edu/ucne/planetsapi/presentation/characters/list/CharacterListViewModel.kt
@@ -0,0 +1,70 @@
+package edu.ucne.planetsapi.presentation.characters.list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.planetsapi.data.remote.Resource
+import edu.ucne.planetsapi.domain.usecase.GetCharactersUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CharacterListViewModel @Inject constructor(
+    private val getCharactersUseCase: GetCharactersUseCase
+) : ViewModel() {
+    private val _state = MutableStateFlow(CharacterListUiState())
+    val state = _state.asStateFlow()
+
+    init {
+        loadCharacters()
+    }
+
+    private fun onEvent(event: CharacterListUiEvent) {
+        when (event) {
+            is CharacterListUiEvent.UpdateFIlters -> _state.update {
+                it.copy(
+                    filterName = event.name,
+                    filterGender = event.gender,
+                    filterRace = event.race
+                )
+            }
+
+            CharacterListUiEvent.Search -> loadCharacters()
+        }
+    }
+
+    private fun loadCharacters() {
+        viewModelScope.launch {
+            val current = _state.value
+
+            getCharactersUseCase(
+                name = current.filterName.takeIf { it.isNotBlank() },
+                gender = current.filterGender.takeIf { it.isNotBlank() },
+                race = current.filterRace.takeIf { it.isNotBlank() }
+            ).collect { result ->
+                when (result) {
+                    is Resource.Loading -> _state.update { it.copy(isLoading = true) }
+
+                    is Resource.Success ->
+                        _state.update {
+                            it.copy(
+                                isLoading = false,
+                                characters = result.data ?: emptyList()
+                            )
+                        }
+
+                    is Resource.Error ->
+                        _state.update {
+                            it.copy(
+                                isLoading = false,
+                                error = result.message
+                            )
+                        }
+                }
+            }
+        }
+    }
+}
